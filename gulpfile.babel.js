57132dfea885ae303bfdd014e7a49f6c79cf79c1
Gyuri-v <fnffkdll@naver.com>
2022-06-22 00:00:13 +0900

    PUG COMPILATION - gulp-pug 사용해보기

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,20 @@
 import gulp from 'gulp';
+import gpug from 'gulp-pug';
+
+const routes = {
+  pug: {
+    src: 'src/**/*.pug',
+    dest: 'build', // 목적지(destination) 설정
+  },
+};
+
+export const pug = () => {
+  // return 해야함
+  // gulp 는 다양한 object를 가짐
+  gulp
+    .src(routes.pug.src) // 변환하고자 하는 파일
+    .pipe(gpug()) // pipe 에 gulp-pug 에서 불러온 함수를 집어넣음
+    .pipe(gulp.dest(routes.pug.dest)); // 목적지(destination) 설정
+};
+
+export const dev = gulp.series([pug]); // task들의 series
