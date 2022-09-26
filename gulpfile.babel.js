f2b800452b888c8e49e3fee2d410339e3cd29965
Gyuri-v <fnffkdll@naver.com>
2022-09-26 21:49:43 +0900

    babel support js 컴파일

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,15 +5,12 @@ import ws from 'gulp-webserver';
 import image from 'gulp-image';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
+import autoprefixed from "gulp-autoprefixer";
+import miniCSS from 'gulp-csso';
+import bro from 'gulp-bro';
+import babelify from 'babelify';
 
 const sass = gulpSass(dartSass);
-
-// import sass from 'gulp-sass';
-
-// sass.compiler = require("node-sass");
-// const sass = require("gulp-sass")(require("node-sass"));
-
-// const sass = require('gulp-sass')(require('sass'));
 
 const routes = {
     pug: {
@@ -30,6 +27,11 @@ const routes = {
         watch: "src/scss/**/*.scss",
         src: "src/scss/style.scss",
         dest: "build/css",
+    },
+    js: {
+        watch: "src/js/**/*.js",
+        src:"src/js/main.js",
+        dest: "build/js"
     }
 };
 
@@ -38,10 +40,32 @@ const pug = () =>
         .src(routes.pug.src)               // 변환하고자 하는 파일
         .pipe(gpug())                      // pipe 에 gulp-pug 에서 불러온 함수를 집어넣음
         .pipe(gulp.dest(routes.pug.dest)); // 목적지(destination) 설정
+
 const clean = () => del(['build']);        // 안에 삭제할 확장자나 폴더 이름을 적어줌
-const webserver = () => gulp.src('build').pipe(ws({ livereload: true, open: true }));
-const img = () => gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
-const styles = () => gulp.src(routes.scss.src).pipe(sass().on('error', sass.logError)).pipe(gulp.dest(routes.scss.dest));
+
+const webserver = () => 
+    gulp.src('build')
+        .pipe(ws({ livereload: true, open: true }));
+
+const img = () => 
+    gulp.src(routes.img.src)
+        .pipe(image())
+        .pipe(gulp.dest(routes.img.dest));
+
+const styles = () => 
+    gulp.src(routes.scss.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixed())
+        .pipe(miniCSS())
+        .pipe(gulp.dest(routes.scss.dest));
+
+const js = () => 
+    gulp.src(routes.js.src).pipe(bro({
+        transform: [
+            babelify.configure({ presets: ['@babel/preset-env'] }),
+            [ 'uglifyify', { global: true } ]
+        ]
+    })).pipe(gulp.dest(routes.js.dest));
 
 const watch = () => {
     // 변경될때마다 실시간 반영
@@ -49,10 +73,11 @@ const watch = () => {
     gulp.watch(routes.pug.watch, pug);
     gulp.watch(routes.img.src, img);
     gulp.watch(routes.scss.watch, styles);
+    gulp.watch(routes.js.watch, js);
 };
 
 const prepare = gulp.series([clean, img]);
-const assets = gulp.series([pug, styles]);
+const assets = gulp.series([pug, styles, js]);
 const postDev = gulp.parallel([webserver, watch]);
 // 동시에 두가지 task 실행을 원한다면, 일렬로 쓰면 안됨 -> seties -> parallel 로 변경
 
